32adc1c743883e000cb6c8882d62de3a74b5d87b
roundspring2003 <hwilly40159@gmail.com>
2026-07-17 15:14:36 +0800

    fix: set mss

--- config/iptables/upf-iptables.sh
+++ config/iptables/upf-iptables.sh
@@ -2,8 +2,5 @@
 
 iptables -I FORWARD 1 -j ACCEPT
 
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
-
 iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
 iptables -A FORWARD -i eth1 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
